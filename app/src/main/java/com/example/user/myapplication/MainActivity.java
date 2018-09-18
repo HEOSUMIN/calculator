fee2cf87e64d83d68205a8ca3a1b90a4c2f3c0c4
HEOSUMIN <gjtnals79@naver.com>
2018-09-18 16:49:26 +0900

    [2018-9-18] Calculator commit

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,24 +2,31 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected double ans=0.;  //Answer
+    protected double ans = 0.;  //Answer
+    protected Button btEqual;
 
-    protected void inputNumber(){
+    protected void inputNumber() {
 
     }
 
-    double op4(double x){
+    protected double op4(String sOp, double x) {
         return 0;
     }
 
-    double mathOp(double x){
+    protected double mathOp(String sFun, double x) {
         return 0;
     }
 
-    void printNum(double x){
+    protected void printNum(double x) {
+
+    }
+
+    protected void calculate() {
 
     }
 
@@ -28,5 +35,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btEqual = (Button) findViewById(R.id.btEqual);
+        btEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
     }
 }
+
